be8070dbe15299a927a4e58717a059df2db5570d
Peter Edwards <peadar@arista.com>
2019-10-04 01:05:07 -0700

    Sometimes we get __kernel_vsyscall on the stack here.

--- tests/basic-test.py
+++ tests/basic-test.py
@@ -12,6 +12,8 @@ assert len(threads) == 1
 thread = threads[0]
 stack = thread["ti_stack"]
 
+if stack[0]["function"] == "__kernel_vsyscall":
+    stack = stack[1:]
 assert stack[0]["function"] == "raise" or stack[0]["function"] == "__GI_raise" or stack[0]["function"] == "gsignal"
 assert stack[1]["function"] == "abort" or stack[1]["function"] == "__GI_abort"
 assert stack[2]["function"] == "g"
