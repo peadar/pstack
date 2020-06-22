d5b3e9244565e0dabce96b8f1ef0b2bf2e51ca47
Peter Edwards <peadar@arista.com>
2020-06-22 18:23:00 +0100

    Don't fail test if symbols for libc aren't installed

--- tests/segv-test.py
+++ tests/segv-test.py
@@ -9,7 +9,14 @@ for cmd in [ "tests/segv", 'tests/segvrt' ]:
    frames = pstack_result[0]["ti_stack"]
 
    (trampolines, symbols, dies) = zip(
-           *((f["trampoline"],f["symbol"]["st_name"], f["die"]) for f in frames) )
+           *((f["trampoline"],
+              f["symbol"],
+              f["die"]) for f in frames) )
+
+   # convert symbols to extrac their names, and filter out where we have no symbol
+   symbols = [ sym["st_name"] for sym in symbols if sym is not None ]
+   print(symbols)
+
    assert any(trampolines)
    assert "my_abort" in dies
    assert 'f' in dies
