bb69147de45d60d14350a11aeeed51744d2f9a48
Nikita Skobelevs <nskobelevs@arista.com>
2021-05-26 14:52:30 +0100

    Fix include for _PyRuntimeState
    _PyRuntimeState is located in internal/pystate.h for Python
    3.7.10.
    
    Py_BUILD_CORE is needed by pyatomic.h which is already included by
    Python.h so it must be defined before then

--- pythonc.c
+++ pythonc.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
-#include "Python.h"
 #define Py_BUILD_CORE
-#include "internal/pycore_pystate.h"
+#include <Python.h>
+#include <internal/pystate.h>
 
 size_t pyInterpOffset() {
    return offsetof(_PyRuntimeState, interpreters) + offsetof(struct pyinterpreters, head);
