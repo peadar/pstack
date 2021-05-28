fbae06cd68dfe438fc6a693b12cab94cd2825c59
Peter Edwards <peadar@isainmdom.net>
2021-05-28 16:29:28 +0000

    Deal with different py3 version internals
    Headers we are interested moved around the py3 source at times. Allow
    for building on at least 3.7, 3.8 and 3.9

--- pythonc.c
+++ pythonc.c
@@ -1,7 +1,13 @@
 #include <stddef.h>
 #define Py_BUILD_CORE
 #include <Python.h>
+#if PY_VERSION_HEX >= 0x03090000
+#include <internal/pycore_pystate.h>
+#elif PY_VERSION_HEX >= 0x3070000
 #include <internal/pystate.h>
+#else
+#error "no support for this version of python"
+#endif
 
 size_t pyInterpOffset() {
    return offsetof(_PyRuntimeState, interpreters) + offsetof(struct pyinterpreters, head);
