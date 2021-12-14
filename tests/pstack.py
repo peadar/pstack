9ed9d2e01eb7d2e793374e47066427be98bfb093
Peter Edwards <peadar@arista.com>
2021-12-14 01:09:24 +0000

    Fix tests when PSTACK_BIN is set

--- tests/pstack.py
+++ tests/pstack.py
@@ -2,10 +2,13 @@ import json
 import subprocess
 import coremonitor
 import sys
+import os
+
+PSTACK_BIN = os.environ.get("PSTACK_BIN", "pstack")
 
 def JSON(cmd, childfunc = None):
     with coremonitor.CoreMonitor( cmd, childfunc ) as cm:
-        args = ["./pstack", "-j" ]
+        args = ["./%s" % PSTACK_BIN, "-j" ]
         if childfunc:
             args.append(sys.executable)
         args.append(cm.core())
@@ -15,5 +18,5 @@ def JSON(cmd, childfunc = None):
 
 def TEXT(cmd):
     with coremonitor.CoreMonitor( cmd, None ) as cm:
-        text = subprocess.check_output(["./pstack", "-a", cm.core()])
+        text = subprocess.check_output(["./%s" % PSTACK_BIN, "-a", cm.core()])
         return text
