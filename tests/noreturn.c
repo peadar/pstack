50e626531255c21434a9680e8c9986edb1ae90f4
Peter Edwards <peadar@arista.com>
2022-07-08 11:30:14 +0100

    Make tests work in gcc11/clang
    When inlined, we lose our stackframe for our trampoline function in this
    test - disallow the optimisations that break the test.

--- tests/noreturn.c
+++ tests/noreturn.c
@@ -6,11 +6,12 @@
 // Because assert_fail is marked as noreturn, the call to it is not followed
 // by another instruction in this function.
 // This test ensures that "thisFunctionWontReturn is on the dumped stack.
+__attribute__((noinline, weak)) // avoid inlining or IPO.
 int thisFunctionWontReturn(int x)
 {
     if (x) {
         for (int i = 0; i < 10; ++i) {
-            printf("%d green bottles hanging on the wall", 10 - i);
+            printf("%d green bottles hanging on the wall\n", 10 - i);
         }
         thisFunctionTerminatesTheProcess();
     }
