a7e2ddaf8e686f1321e77700393b9f3f4c57f751
Peter Edwards <peadar@arista.com>
2022-07-25 16:43:11 +0100

    Fix JSON/TEXT formatting issue

--- tests/pstack.py
+++ tests/pstack.py
@@ -9,28 +9,26 @@ import tempfile
 PSTACK_BIN = os.environ.get("PSTACK_BIN", "pstack")
 DO_CORE = os.environ.get("PSTACK_TEST_USECORE", None)
 
-def JSON(cmd):
-    args = ["./%s" % PSTACK_BIN, "-j" ]
+def _run(cmd, args):
     if DO_CORE:
         with coremonitor.CoreMonitor(cmd) as cm:
-            args.append(scope.core())
-            text = subprocess.check_output(args, universal_newlines=True)
-            j = json.loads( text )
-            return j, None #scope.output
+            args.append(cm.core())
+            pstackOutput = subprocess.check_output(args, universal_newlines=True)
+            return pstackOutput, cm.output
     else:
         fd, fname = tempfile.mkstemp()
-        fp = os.fdopen(fd, "r")
+        pstackOutput = os.fdopen(fd, "r")
         args.append("-o")
         args.append(fname)
         args.append("-x")
         args.append(" ".join(cmd))
-        text = subprocess.check_output(args, universal_newlines=True)
-        jtext = fp.read()
-        j = json.loads( jtext )
-        return j, text
+        programOutput = subprocess.check_output(args, universal_newlines=True)
+        os.remove( fname )
+        return pstackOutput.read(), programOutput
 
 def TEXT(cmd):
-    with coremonitor.CoreMonitor(cmd) as cm:
-        text = subprocess.check_output(["./%s" % PSTACK_BIN, "-a", cm.core()],
-                universal_newlines=True)
-        return text, cm.output
+    return _run(cmd, ["./%s" % PSTACK_BIN, "-a"])
+
+def JSON(cmd):
+    pstack, target = _run(cmd, ["./%s" % PSTACK_BIN, "-j" ])
+    return json.loads(pstack), target
