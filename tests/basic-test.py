96486a35759fb9386ba24cd60525c65ed4daa208
Peter Edwards <peadar@arista.com>
2023-06-09 11:35:57 +0100

    Fix tests on ubuntu

--- tests/basic-test.py
+++ tests/basic-test.py
@@ -10,7 +10,7 @@ for ex in [ "basic", "basic-zlib", "basic-zlib-gnu" ]:
     thread = threads[0]
     stack = thread["ti_stack"]
 
-    impl_details = set(["__kernel_vsyscall", "__pthread_kill", "pthread_kill", "__pthread_kill_implementation"])
+    impl_details = set(["__kernel_vsyscall", "__pthread_kill", "pthread_kill", "__pthread_kill_implementation", "__GI___pthread_kill" ])
     while "symbol" in stack[0] and stack[0]["symbol"]["st_name"] in impl_details:
         stack.pop(0)
     assert stack[0]["symbol"]["st_name"] == "raise" or stack[0]["symbol"]["st_name"] == "__GI_raise" or stack[0]["symbol"]["st_name"] == "gsignal"
