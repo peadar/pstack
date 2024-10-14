52c93132663bebf651996d9e39e57eeed69b5043
Peter Edwards <peadar@arista.com>
2024-10-14 21:52:43 +0100

    Use basic test to verify frame-pointer fallback unwinding

--- tests/basic-test.py
+++ tests/basic-test.py
@@ -3,7 +3,7 @@
 import pstack
 
 
-for ex in [ "basic", "basic-zlib", "basic-zlib-gnu" ]:
+for ex in [ "basic", "basic-zlib", "basic-zlib-gnu", "basic-no-unwind" ]:
     print("running for '%s'" % ex)
     threads, _ = pstack.JSON(["tests/%s" % ex ])
     assert len(threads) == 1
@@ -15,5 +15,8 @@ for ex in [ "basic", "basic-zlib", "basic-zlib-gnu" ]:
         stack.pop(0)
     stack.pop(0)
     # Assert we get the DIE etc from the g and f functions under abort.
-    assert stack[0]["die"] == "g" and stack[0]["symbol"]["st_name"] == "g"
-    assert stack[1]["die"] == "f" and stack[1]["symbol"]["st_name"] == "f"
+    if "no-unwind" not in ex:
+       assert stack[0]["die"] == "g" and stack[0]["symbol"]["st_name"] == "g"
+       assert stack[1]["die"] == "f" and stack[1]["symbol"]["st_name"] == "f"
+    assert stack[0]["symbol"]["st_name"] == "g"
+    assert stack[1]["symbol"]["st_name"] == "f"
