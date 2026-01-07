377587d5a6d5c3b23e4b202c8488880797bb9399
Peter Edwards <peadar@arista.com>
2026-01-07 11:35:22 +0000

    progress with offsets

--- mkpyoff.cc
+++ mkpyoff.cc
@@ -3,6 +3,7 @@
 #include "libpstack/dwarf.h"
 #include <cassert>
 #include <iostream>
+#include <utility>
 
 namespace pstack { namespace {
 
@@ -11,16 +12,43 @@ namespace pstack { namespace {
     exit(1);
 }
 
-Dwarf::DIE findDIE(pstack::Dwarf::DIE &die, std::string_view name) {
-    if (die.name() == name) {
+struct Pad {
+    size_t cnt;
+    static std::array<char, 4096> spaces;
+};
+
+std::array<char, 4096> Pad::spaces = []() {
+        std::array<char, 4096 > spaces_;
+        spaces_.fill(' ');
+        return spaces_;
+}();
+
+std::ostream & operator << (std::ostream &os, const Pad &pad) {
+    return os << std::string_view{ Pad::spaces.end() - std::min(pad.cnt * 4, Pad::spaces.size()), Pad::spaces.end() };
+}
+
+struct BinDump {
+    const Reader::csptr &io;
+    off_t offset;
+    const Dwarf::DIE &type;
+    size_t depth{};
+    BinDump(const Reader::csptr &io_, off_t offset_, const Dwarf::DIE &type_, size_t depth_ = 0)
+        : io{io_} , offset{offset_} , type{type_}, depth{depth_} { }
+    void dump(std::ostream &os) const;
+    void dumpBase(std::ostream &os) const;
+    void dumpStructFields(std::ostream &os) const;
+};
+
+Dwarf::DIE findDIE(const pstack::Dwarf::DIE &die, std::string_view name) {
+    if (die.name() == name)
         return die;
-    }
     for (auto child : die.children()) {
-        return findDIE(child, name);
+        auto found = findDIE(child, name);
+        if (found)
+            return found;
     }
     return {};
 }
-
 
 Dwarf::DIE findDIE(const pstack::Dwarf::Unit::sptr &unit, std::string_view name) {
     return findDIE(unit->root(), name);
@@ -28,11 +56,76 @@ Dwarf::DIE findDIE(const pstack::Dwarf::Unit::sptr &unit, std::string_view name)
 
 Dwarf::DIE findDIE(const pstack::Dwarf::Info::sptr &info, std::string_view name) {
     for (auto unit : info->getUnits()) {
-        DIE rv = findDIE(unit, name);
+        Dwarf::DIE rv = findDIE(unit, name);
         if (rv)
             return rv;
     }
     return Dwarf::DIE();
+}
+
+
+std::ostream &operator << (std::ostream &os, const BinDump &bd) {
+    bd.dump(os);
+    return os;
+}
+
+
+void BinDump::dumpBase(std::ostream &os) const {
+    auto encoding = Dwarf::Encoding(uintptr_t(type.attribute(Dwarf::DW_AT_encoding)));
+    switch (encoding) {
+        case Dwarf::DW_ATE_unsigned:
+            switch (uintptr_t(type.attribute(Dwarf::DW_AT_byte_size))) {
+                case 4:
+                    os << io->readObj<uint32_t>(offset);
+                    break;
+                case 8:
+                    os << io->readObj<uint64_t>(offset);
+                    break;
+                case 2:
+                    os << io->readObj<uint16_t>(offset);
+                default:
+                    abort();
+            }
+            break;
+        default:
+            abort();
+    }
+}
+
+void
+BinDump::dump(std::ostream &os) const {
+    switch (type.tag()) {
+        case Dwarf::DW_TAG_structure_type:
+            dumpStructFields(os);
+            break;
+        case Dwarf::DW_TAG_typedef:
+            os << BinDump(io, offset, Dwarf::DIE(type.attribute(Dwarf::DW_AT_type)), depth);
+            break;
+
+        case Dwarf::DW_TAG_base_type: {
+            dumpBase(os);
+            break;
+
+        }
+
+        default:
+            os << Pad{depth} << type.tag() << "/" << type.name() << "@" << offset;
+            break;
+    }
+}
+
+void
+BinDump::dumpStructFields(std::ostream &os) const {
+    for (auto &member : type.children()) {
+        if (member.tag() != Dwarf::DW_TAG_member)
+            continue;
+        os << Pad{depth} << member.name() << ": ";
+        auto chtype = Dwarf::DIE(member.attribute(Dwarf::DW_AT_type));
+        auto fieldOff = member.attribute(Dwarf::DW_AT_data_member_location);
+        assert(fieldOff.valid());
+        os << BinDump(io, offset + uintptr_t(fieldOff), chtype, depth + 1) << "\n";
+    }
+    // assert(end == offset);
 }
 
 int main(int argc, char *argv[]) {
@@ -56,6 +149,15 @@ int main(int argc, char *argv[]) {
     auto dwarf = ctx.findDwarf(elf);
 
     auto runtimeType = findDIE(dwarf, "_Py_DebugOffsets");
+    if (!runtimeType) {
+        dwarf = dwarf->getAltDwarf();
+        if (dwarf)
+            runtimeType = findDIE(dwarf, "_Py_DebugOffsets");
+    }
+    assert(runtimeType);
+    std::cout << BinDump(pyRuntimeSec.io(), 0, runtimeType, 0);
+
+    std::cout << json(runtimeType);
     return 0;
 }
 
